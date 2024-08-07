4f9d3c09d9458e8ccec0ea6b829ebabe0bab1466
tamir avraham <tamir.avrah@gmail.com>
2024-08-07 15:56:50 -0700

    fixed more errors

--- src/cpu.rs
+++ src/cpu.rs
@@ -104,12 +104,12 @@ impl Cpu {
     fn dec_8bit_register(&mut self, register8bit: Register8bit) {
         let register = self.get_register_refm_from_register_8bit(register8bit);
         let new_register = register.wrapping_sub(1);
+        *register = new_register;
         self.registers
             .flag(Z, new_register == 0)
             .flag(N, true)
             .flag(H, (new_register & 0x0F) == 0);
         self.clock.m += 4;
-        *register = new_register;
     }
     #[inline]
     fn dec_16bit_register(&mut self, register16bit: Register16bit) {
@@ -137,26 +137,29 @@ impl Cpu {
     #[inline]
     fn add_8bit_value(&mut self, value: u8, use_carry: bool) {
         let carry = self.use_carry(use_carry);
-        let new_a = self.registers.a.wrapping_add(value).wrapping_add(carry);
+        let a =self.registers.a;
+        let new_a = a.wrapping_add(value).wrapping_add(carry);
+
         self.registers
             .flag(Z, new_a == 0)
             .flag(N, false)
-            .flag(H, (self.registers.a & 0xF) + (value & 0xF) + carry > 0xF)
+            .flag(H, (a & 0xF) + (value & 0xF) + carry > 0xF)
             .flag(
                 C,
-                (self.registers.a as u16) + (value as u16) + (carry as u16) > 0xFF,
+                (a as u16) + (value as u16) + (carry as u16) > 0xFF,
             );
         self.clock.m += 4;
     }
     #[inline]
     fn add_16bit_value(&mut self, value: u16) {
-        let new_hl = self.registers.get_hl().wrapping_add(value);
+        let hl = self.registers.get_hl();
+        let new_hl = hl.wrapping_add(value);
         self.registers
             .flag(
                 H,
-                (self.registers.get_hl() & 0x07FF) + (value & 0x7FF) > 0x07FF,
+                (hl & 0x07FF) + (value & 0x7FF) > 0x07FF,
             )
-            .flag(C, self.registers.get_hl() > 0xFFFF - value)
+            .flag(C, hl > 0xFFFF - value)
             .flag(N, false);
         self.registers.write_hl(new_hl);
         self.clock.m += 8;
@@ -164,13 +167,14 @@ impl Cpu {
     #[inline]
     fn sub_8bit_value(&mut self, value: u8, use_carry: bool) {
         let carry = self.use_carry(use_carry);
-        let new_a = self.registers.a.wrapping_add(value).wrapping_add(carry);
+        let a =self.registers.a;
+        let new_a = a.wrapping_add(value).wrapping_add(carry);
         self.registers
             .flag(Z, new_a == 0)
-            .flag(H, (self.registers.a & 0x0F) < (value & 0x0F) + carry)
+            .flag(H, (a & 0x0F) < (value & 0x0F) + carry)
             .flag(
                 C,
-                (self.registers.a as u16) < (value as u16) + (carry as u16),
+                (a as u16) < (value as u16) + (carry as u16),
             )
             .flag(N, true);
         self.registers.a = new_a;
